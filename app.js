4323ef735273ca2d252013b8571e55dedd4715b6
Matthew Curran <five3183@gmail.com>
2018-10-08 10:40:23 -0400

    basic working game

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ Game Function:
 // Game values
 let min = 1
 let max = 10
-let winningNum = 2
+
+//  Get the difference of the min and the max numbers and add 1 to account for all numbers in the range.  
+let range = max - min + 1
+
+// Set the winning number to a random number that is based off the range. Since math.random can be 0 we add min to make the minimum possible value incase random returns 0. 
+let winningNum = Math.floor(Math.random()*(range) + min)
 let guessesLeft = 3
 
 // UI Elements
@@ -31,6 +36,13 @@ const setMessage = (msg, color) => {
    message.textContent = msg
 }
 
+// Play again event listener:  Here we target the parent element to check if there is a className for play-again that was added after the paged loaded. Click cannot be used here, because as soon as the mouse is released it will trigger the reload.  We one this to happen on mouse down. 
+game.addEventListener('mousedown', (e) => {
+   if(e.target.className === 'play-again') {
+      window.location.reload()
+   }
+})
+
 // GAME OVER FUNCTION
 const gameOver = (win, msg) => {
    let color
@@ -39,11 +51,15 @@ const gameOver = (win, msg) => {
    guessInput.disabled = true
    guessInput.style.borderColor = color
    setMessage(msg, color)
+
+   // Play again?  Here will will chance the inner text on the submit button to play again. And add a class to it so that we can reload the page on click.  
+   guessBtn.value = 'Play Again'
+   guessBtn.className += 'play-again'
 }
 // listen for guess 
 guessBtn.addEventListener('click', () => {
    let guess = parseInt(guessInput.value)
-   console.log(guess)
+   // console.log(guess)
    if(isNaN(guess) || guess < min || guess > max) {
       setMessage(`Please enter numbers between ${min} and ${max}`, 'red')
    }
